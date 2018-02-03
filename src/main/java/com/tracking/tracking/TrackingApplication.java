eaefb8a58afa59b01d2b9bad9ad44320a563ae6a
ivanss <vito_ivan_1@hotmail.com>
2018-02-03 11:35:26 -0500

    adding comments

--- src/main/java/com/tracking/tracking/TrackingApplication.java
+++ src/main/java/com/tracking/tracking/TrackingApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrackingApplication {
 
 	//docs
+	//docs
 	public static void main(String[] args) {
 		SpringApplication.run(TrackingApplication.class, args);
 	}
