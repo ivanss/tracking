4ce4fcbcc112bd2d8f53c1f724db0def12002eb2
ivanss <vito_ivan_1@hotmail.com>
2018-02-03 11:23:55 -0500

    solving comments

--- src/main/java/com/tracking/tracking/TrackingApplication.java
+++ src/main/java/com/tracking/tracking/TrackingApplication.java
@@ -3,9 +3,11 @@ package com.tracking.tracking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//docs
 @SpringBootApplication
 public class TrackingApplication {
 
+	//docs
 	public static void main(String[] args) {
 		SpringApplication.run(TrackingApplication.class, args);
 	}
